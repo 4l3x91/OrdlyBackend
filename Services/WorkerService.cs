8da786dc5731674d508a45cb873cfa821a6aa58f
Zaai90 <erik.grune@gmail.com>
2022-07-17 01:57:47 +0200

    fix WorkerService

--- Services/WorkerService.cs
+++ Services/WorkerService.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using OrdlyBackend.Interfaces;
 using OrdlyBackend.Models;
 
@@ -5,22 +6,29 @@ namespace OrdlyBackend.Services;
 
 public class WorkerService : BackgroundService
 {
-    private DateTime _DateLastRun;
+    private DateTime? _DateLastRun;
     private bool inProgress = false;
+    private ILogger<WorkerService> _logger;
 
     private readonly IServiceScopeFactory _serviceScopeFactory;
-    public WorkerService(IServiceScopeFactory serviceScopeFactory)
+    public WorkerService(IServiceScopeFactory serviceScopeFactory, ILogger<WorkerService> logger)
     {
+        _logger = logger;
         _serviceScopeFactory = serviceScopeFactory;
-        _DateLastRun = DateTime.Now.Date;
+        _DateLastRun = null;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        if (!_DateLastRun.HasValue)
+        {
+            await InitiateLastRun();
+        }
+
         while (!stoppingToken.IsCancellationRequested)
         {
             if(!inProgress) await SetDailyWordAsync();
-            await Task.Delay(5000);
+            await Task.Delay(60000);
         }
     }
 
@@ -36,30 +44,39 @@ public class WorkerService : BackgroundService
                 var wordService = scope.ServiceProvider.GetRequiredService<IWordService>();
 
                 Word choosenWord = null;
-                while(choosenWord == null)
+                while (choosenWord == null)
                 {
-                   choosenWord = await GetWordAsync(wordService, dailyWordService);
+                    choosenWord = await GetWordAsync(wordService, dailyWordService);
                 }
-                var dailyWord = new DailyWord() { WordId = choosenWord.WordId, Date = DateTime.Now };
+                var dailyWord = new DailyWord() { WordId = choosenWord.WordId, Date = DateTime.Now.Date };
 
-                dailyWordService.AddNewDailyWord(dailyWord);
+                await dailyWordService.AddNewDailyWordAsync(dailyWord);
             }
+            _DateLastRun = DateTime.Now.Date;
+            _logger.LogInformation("WorkerService succeded at " + DateTime.Now);
+        }
+              inProgress = false;
+    }
 
-                _DateLastRun = DateTime.Now.Date;
-                inProgress = false;
+    private async Task InitiateLastRun()
+    {
+        using (var scope = _serviceScopeFactory.CreateScope())
+        {
+            var dailyWordService = scope.ServiceProvider.GetRequiredService<IDailyWordService>();
+            var lastDailys = await dailyWordService.GetLatestDailysAsync();
+            var lastDaily = lastDailys.First();
+            _DateLastRun = lastDaily.Date.Date;
         }
     }
 
     private async Task<Word> GetWordAsync(IWordService wordService, IDailyWordService dailyWordService)
     {
         var randomWord = await wordService.GetRandomWordAsync();
-        var latestWords = await dailyWordService.GetLatestDailys();
+        var latestWords = await dailyWordService.GetLatestDailysAsync();
         if(latestWords.Any(x=> x.WordId == randomWord.WordId))
         {
             return null;
         }
-
         return randomWord;
-
     }
 }
