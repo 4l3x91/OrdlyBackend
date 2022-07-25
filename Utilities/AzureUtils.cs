6414eefd0a1b83523a26d7ee3acdf7677ca698e7
Zaai90 <erik.grune@gmail.com>
2022-07-25 20:43:59 +0200

    fix

--- Utilities/AzureUtils.cs
+++ Utilities/AzureUtils.cs
@@ -5,8 +5,7 @@ namespace OrdlyBackend.Utilities
 {
     public static class AzureUtils
     {
-        static Uri keyVaultEndpoint = new Uri("https://ordlysecrets.vault.azure.net/");
-        //private static Uri keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+        private static Uri keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
         private static SecretClient client = new SecretClient(keyVaultEndpoint, new DefaultAzureCredential());
         public static string GetSecretFromVault(string key)
         {
