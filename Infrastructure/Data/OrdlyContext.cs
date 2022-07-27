da5f6bd791d66836d0101687ad0b0c9da906517a
4l3x <alexander.auermaa@gmail.com>
2022-07-27 02:16:49 +0200

    DbSets for Ranks and UserRanks added

--- Infrastructure/Data/OrdlyContext.cs
+++ Infrastructure/Data/OrdlyContext.cs
@@ -5,11 +5,13 @@ namespace OrdlyBackend.Infrastructure.Data;
 
 public class OrdlyContext : DbContext
 {
-        public DbSet<User> Users { get; set; }
+    public DbSet<User> Users { get; set; }
     public DbSet<Word> Words { get; set; }
     public DbSet<DailyWord> DailyWords { get; set; }
     public DbSet<UserGame> UserGames { get; set; }
     public DbSet<Guess> Guesses { get; set; }
+    public DbSet<Rank> Ranks { get; set; }
+    public DbSet<UserRank> UserRanks { get; set; }
     public OrdlyContext(DbContextOptions<OrdlyContext> options) : base(options)
     {
      
